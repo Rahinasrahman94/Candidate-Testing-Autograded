1492f992be94cfd7225981e837f496b99c330e2b
rahinasrahman <rshinasrahman94@gmail.com>
2024-05-28 16:36:50 -0700

    graded full assignment with spacing

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,7 +12,7 @@ let correctAnswer="Sally Ride";
 let candidateAnswer="";
 
 //TODO: Variables for Part 2
-let questions=["Who was the first American woman in space? ","True or false : 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
+let questions=["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
 let correctAnswers=["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers=[];
 
@@ -37,10 +37,9 @@ console.log(`Correct Answer : ${correctAnswers[i]}`);
 }
 
 function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-let numberofCorrectAnswers=0;
-let numberofQuizQuestions  = questions.length;
+// TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+let numberofCorrectAnswers = 0;
+let numberofQuizQuestions = questions.length;
 for (let i = 0; i < questions.length ; i++)
 {
   if(correctAnswers[i].toUpperCase() == candidateAnswers[i].toUpperCase())
@@ -52,19 +51,19 @@ for (let i = 0; i < questions.length ; i++)
   
 }
   else{
-    console.log("Question "  + (i+1) + " : InCorrect");
+    console.log("Question " + (i+1) + " : InCorrect");
   }
 }
   let grade = (numberofCorrectAnswers/numberofQuizQuestions) * 100 ;  //TODO 3.2 use this variable to calculate the candidates score.
  //console.log(grade);
 if(grade>=80)
 {
-  console.log(">>> Overall Grade : "+grade+"(" +numberofCorrectAnswers+ "of 5 responses correct) <<<");
+  console.log(">>> Overall Grade : "+grade+"(" +numberofCorrectAnswers+  "of 5 responses correct) <<<");
    console.log(">>> Status : PASSED <<<");
    
 }
 else{
-  console.log(">>> Overall Grade:"+grade+"("  +numberofCorrectAnswers+     "of 5 responses correct) <<<");
+  console.log(">>> Overall Grade:"+grade+"(" +numberofCorrectAnswers+     "of 5 responses correct) <<<");
   console.log(">>> Status : FAILED <<<");
 }
   return grade; 
